96cb4530bc4a628f82a354a0f419d0bde22b862c
Myoung <mhbae93@gmail.com>
2022-03-07 15:14:43 +0000

    initial commit + header with logo

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
