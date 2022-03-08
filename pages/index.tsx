252a937c0b15793d9547e1f81172398364d483b3
Myoung <mhbae93@gmail.com>
2022-03-08 04:37:21 +0000

    refactor banner to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home = () => {
@@ -9,25 +10,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="flex items-center justify-between border-y-black bg-yellow-300 py-10 lg:py-0">
-        <div className="space-y-5 px-10">
-          <h1 className="max-w-xl font-serif text-6xl">
-            <span className="underline decoration-black decoration-4">
-              Medium
-            </span>{' '}
-            is a place to write, read, and connect
-          </h1>
-          <h2>
-            It's easy and free to post your thinking on any topic and connect
-            with millions of readers.
-          </h2>
-        </div>
-        <img
-          className="hidden h-32 md:inline-flex lg:h-full"
-          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
-          alt=""
-        />
-      </div>
+      <Banner />
     </div>
   )
 }
