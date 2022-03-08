eaccc24026caa3d13c909bf4b3cb476dd930d7b2
Myoung <mhbae93@gmail.com>
2022-03-08 13:17:24 +0000

    post paths created using slug from sanity.io. The cache is updated every 60 seconds

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,13 @@
+import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
 
-function Post() {
+interface Props {
+  post: Post
+}
+
+function Post({ post }: Props) {
   return (
     <main>
       <Header />
@@ -26,4 +32,47 @@ export const getStaticPaths = async () => {
       slug: post.slug.current,
     },
   }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    author-> {
+      name,
+      image
+    },
+    'comments': *[
+      _type == "comment" &&
+      post._ref == ^._id &&
+      approved == true
+    ],
+    description,
+    mainImage,
+    slug,
+    body
+  }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60,
+  }
 }
