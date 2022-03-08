3115bc8574404f808bf7956232570f6acb7fcb3e
Myoung <mhbae93@gmail.com>
2022-03-08 15:33:43 +0000

    amended styling and links to personal socials

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,24 +1,18 @@
 const Banner = () => {
   return (
-    <div className="flex items-center justify-between border-y-black bg-yellow-300 py-10 lg:py-0">
-    <div className="space-y-5 px-10">
-      <h1 className="max-w-xl font-serif text-6xl">
-        <span className="underline decoration-black decoration-4">
-          Medium
-        </span>{' '}
-        is a place to write, read, and connect
-      </h1>
-      <h2>
-        It's easy and free to post your thinking on any topic and connect
-        with millions of readers.
-      </h2>
+    <div className="flex items-center justify-between border-y-black bg-yellow-300 py-10 lg:py-10">
+      <div className="space-y-5 px-10">
+        <h1 className="max-w-xl font-serif text-6xl">
+          <span className="underline decoration-black decoration-4">
+            Learning Journal
+          </span>{' '}
+          of my software development journey.
+        </h1>
+        <h2>
+          This is a blog where I write down my thoughts about software development
+        </h2>
+      </div>
     </div>
-    <img
-      className="hidden h-32 md:inline-flex lg:h-full"
-      src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
-      alt=""
-    />
-  </div>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,30 +2,33 @@ import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-  return (
+  return ( 
     <header className="mx-auto flex max-w-7xl justify-between p-5">
       <div className="flex items-center space-x-5">
         <Link href="/">
-          <img
-            className="w-44 cursor-pointer object-contain"
-            src="https://links.papareact.com/yvf"
-            alt="medium logo"
-          />
+          <a className="text-4xl text-yellow-500 underline">MHB</a>
         </Link>
-        <div className="hidden items-center space-x-5 md:inline-flex">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
-            Follow
-          </h3>
-        </div>
       </div>
 
-      <div className="flex items-center space-x-5 text-green-600">
-        <h3>Sign In</h3>
-        <h3 className="rounded-full border border-green-600 px-4 py-1">
-          Get Started
-        </h3>
+      <div className="flex items-center space-x-5 text-yellow-600">
+        <a
+          href="https://portfolio-myoung.vercel.app/"
+          className="rounded-full border border-yellow-600 px-4 py-1"
+        >
+          Portfolio
+        </a>
+        <a
+          href="https://github.com/mhbae-dev"
+          className="rounded-full border border-yellow-600 px-4 py-1"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/myoung-bae/"
+          className="rounded-full border border-yellow-600 px-4 py-1"
+        >
+          LinkedIn
+        </a>
       </div>
     </header>
   )
