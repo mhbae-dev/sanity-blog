524f54a2704b879bb4482d06a2d3af92b52b52cb
Myoung <mhbae93@gmail.com>
2022-03-08 04:35:58 +0000

    created banner section

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
+    <header className="mx-auto flex max-w-7xl justify-between p-5">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
@@ -15,10 +15,18 @@ const Header = () => {
         <div className="hidden items-center space-x-5 md:inline-flex">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3 className="bg-green-600 px-4 py-1 text-white rounded-full">Follow</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
         </div>
       </div>
-      <div></div>
+
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="rounded-full border border-green-600 px-4 py-1">
+          Get Started
+        </h3>
+      </div>
     </header>
   )
 }
